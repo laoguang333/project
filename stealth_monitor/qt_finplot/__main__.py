f8d6edc1ede33cc884c935c148d6c4b6e16d9d04
nillouise <614070410@qq.com>
2025-10-15 19:17:44 +0800

    处理报错

--- stealth_monitor/qt_finplot/__main__.py
+++ stealth_monitor/qt_finplot/__main__.py
@@ -1,3 +1,15 @@
+# 在所有导入之前设置DPI感知和环境变量
+import os
+os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "0"
+os.environ["QT_LOGGING_RULES"] = "qt.qpa.window=false"
+
+# 设置DPI感知以解决Qt警告
+try:
+    import ctypes
+    ctypes.windll.shcore.SetProcessDpiAwareness(2)  # PROCESS_PER_MONITOR_DPI_AWARE
+except:
+    pass
+
 from .app import run
 
 
